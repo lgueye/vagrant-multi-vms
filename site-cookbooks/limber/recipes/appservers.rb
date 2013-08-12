5c34c289be99fc2a0ed78a07e252fcf227647880
louis gueye <louis.gueye@figarocms.fr>
2013-08-12 18:25:17 +0200

    Fix: use '-O' option with wget command, test links presence before creating link, remove template 'variables' in order to use node attributes directly in the template

--- site-cookbooks/limber/recipes/appservers.rb
+++ site-cookbooks/limber/recipes/appservers.rb
@@ -1,3 +1,5 @@
+include_recipe "java"
+include_recipe "jetty"
 
 package "libmysql-java" do
   action :install
@@ -11,29 +13,33 @@ bash "install tomcat-jdbc" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  wget http://search.maven.org/remotecontent?filepath=org/apache/tomcat/tomcat-jdbc/7.0.42/tomcat-jdbc-7.0.42.jar
+  wget http://search.maven.org/remotecontent?filepath=org/apache/tomcat/tomcat-jdbc/7.0.42/tomcat-jdbc-7.0.42.jar -O tomcat-jdbc-7.0.42.jar
   cp tomcat-jdbc-7.0.42.jar /usr/share/java
   ln -s /usr/share/java/tomcat-jdbc-7.0.42.jar /usr/share/java/tomcat-jdbc.jar
-  chmod 775 /usr/share/java/tomcat-jdbc.jar
+  chmod 0770 /usr/share/java/tomcat-jdbc.jar
   EOH
 end
 
 bash "update jetty lib directory" do
   user "root"
   code <<-EOH
-  ln -s /usr/share/java/tomcat-jdbc.jar /usr/share/jetty/lib/tomcat-jdbc.jar
-  ln -s /usr/share/java/mysql-connector-java.jar /usr/share/jetty/lib/mysql-connector-java.jar
+  if test ! -h /usr/share/jetty/lib/tomcat-jdbc.jar
+  then
+    ln -s /usr/share/java/tomcat-jdbc.jar /usr/share/jetty/lib/tomcat-jdbc.jar
+  fi
+
+  if test ! -h /usr/share/jetty/lib/mysql-connector-java.jar
+  then
+    ln -s /usr/share/java/mysql-connector-java.jar /usr/share/jetty/lib/mysql-connector-java.jar
+  fi
   EOH
 end
 
 template "/etc/jetty/jetty.xml" do
   source "jetty.xml.erb"
-  mode 0440
+  mode 0770
   owner "root"
   group "root"
-  variables({
-    :jetty_external_libs => ["/usr/share/java/mysql-connector-java.jar", "/usr/share/java/tomcat-jdbc.jar"]
-  })
   notifies :restart, "service[jetty]", :immediately
 end
 
