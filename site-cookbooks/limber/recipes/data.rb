96a049c12c611c80f68279cf1268ccbc52814cc7
louis gueye <louis.gueye@figarocms.fr>
2013-08-12 18:20:46 +0200

    Refactor: remove unnecessary double quotes, reference new attributes names

--- site-cookbooks/limber/recipes/data.rb
+++ site-cookbooks/limber/recipes/data.rb
@@ -4,15 +4,15 @@ include_recipe "database::mysql"
 mysql_connection = {:host => "localhost", :username => 'root',
                     :password => node['mysql']['server_root_password']}
 
-mysql_database node['app']['db_schema'] do
+mysql_database node['app']['db']['schema'] do
   connection mysql_connection
   action :create
 end
 
-mysql_database_user node['app']['db_user'] do
+mysql_database_user node['app']['db']['user'] do
   connection mysql_connection
-  password node['app']['db_password']
-  database_name node['app']['db_schema']
+  password node['app']['db']['password']
+  database_name node['app']['db']['schema']
   host '%'
   privileges [:select, :update, :insert, :delete]
   action [:create, :grant]
