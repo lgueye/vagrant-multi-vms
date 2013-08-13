b8e7617e769dea2577d10e30321e22d65b0f6ab6
louis gueye <louis.gueye@figarocms.fr>
2013-08-13 18:25:57 +0200

    Add search support: customize search attributes

---
+++ site-cookbooks/limber/attributes/search.rb
@@ -0,0 +1,10 @@
+include_attribute 'elasticsearch::default'
+include_attribute 'limber::default'
+
+node.set['java']['jdk_version'] = 7
+node.set['java']['install_flavor'] = 'oracle'
+node.set['java']['oracle']['accept_oracle_download_terms'] = true
+
+node.set['elasticsearch'][:cluster][:name] = node[:app][:name]
+node.set['elasticsearch'][:discovery][:zen][:ping][:multicast][:enabled] = false
+node.set['elasticsearch'][:bootstrap][:mlockall] = false
