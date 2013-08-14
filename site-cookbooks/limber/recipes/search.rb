e1340672a039af1f4d1f40feb62ea665788d944a
louis gueye <louis.gueye@figarocms.fr>
2013-08-14 13:00:30 +0200

    Add search support: configure elasticsearch.yml

--- site-cookbooks/limber/recipes/search.rb
+++ site-cookbooks/limber/recipes/search.rb
@@ -1,8 +1,8 @@
-include_recipe "java"
+include_recipe 'java'
 
-bash "fix elasticsearch outdated jdk seeking algorithm" do
-  user "root"
-  cwd "/tmp"
+bash 'fix elasticsearch outdated jdk seeking algorithm' do
+  user 'root'
+  cwd '/tmp'
   code <<-EOH
   if test ! -h /usr/lib/jvm/java-7-oracle
   then
@@ -12,3 +12,10 @@ bash "fix elasticsearch outdated jdk seeking algorithm" do
 end
 
 include_recipe 'elasticsearch::deb'
+
+template '/etc/elasticsearch/elasticsearch.yml' do
+  source 'elasticsearch.yml.erb'
+  mode '0644'
+  owner 'root'
+  group 'root'
+end
