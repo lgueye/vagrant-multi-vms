397d19585e8cb1aa58850916b0b58f5bfeb178da
louis gueye <louis.gueye@figarocms.fr>
2013-08-27 13:57:20 +0200

    Refactor: Remove unused imports Split user create and grants

--- site-cookbooks/limber/recipes/data.rb
+++ site-cookbooks/limber/recipes/data.rb
@@ -1,8 +1,8 @@
 include_recipe 'limber::default'
-include_recipe 'mysql55::server'
 include_recipe 'database::mysql'
 
-mysql_connection = {:host => 'localhost', :username => 'root',
+mysql_connection = {:host => 'localhost',
+                    :username => 'root',
                     :password => node['mysql']['server_root_password']}
 
 mysql_database node['app']['db']['schema'] do
@@ -14,9 +14,25 @@ mysql_database_user node['app']['db']['user'] do
   connection mysql_connection
   password node['app']['db']['password']
   database_name node['app']['db']['schema']
+  action [:create]
+end
+
+mysql_database_user node['app']['db']['user'] do
+  connection mysql_connection
+  password node['app']['db']['password']
+  database_name node['app']['db']['schema']
+  host 'localhost'
+  privileges [:select, :update, :insert, :delete]
+  action [:grant]
+end
+
+mysql_database_user node['app']['db']['user'] do
+  connection mysql_connection
+  password node['app']['db']['password']
+  database_name node['app']['db']['schema']
   host '%'
   privileges [:select, :update, :insert, :delete]
-  action [:create, :grant]
+  action [:grant]
 end
 
 include_recipe 'minitest-handler'
